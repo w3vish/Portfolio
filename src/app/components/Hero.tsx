15927f1fd79aef0c411b7a92e04d9a31b460650a
Vishal Suryavanshi <w3vish@gmail.com>
2024-12-01 06:09:31 +0530

    replace Vishal Suryavanshi => Viśāl Sūryavaṁśī

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,7 +3,7 @@ const Hero = () => {
     <main>
       <h1 className="mt-4 font-medium text-2xl">Vishal <span className="text-primary">Suryavanshi :)</span></h1>
       <p className="mt-2 text-sm sm:text-base">
-        I am Vishal Suryavanshi, a full-stack web developer with expertise in the MERN stack (MongoDB, Express.js, React, Node.js) as well as SQL databases. I specialize in creating dynamic and user-friendly websites using modern tools and technologies.
+        I am Viśāl Sūryavaṁśī, a full-stack web developer with expertise in the MERN stack (MongoDB, Express.js, React, Node.js) as well as SQL databases. I specialize in creating dynamic and user-friendly websites using modern tools and technologies.
       </p>
     </main>
   );
